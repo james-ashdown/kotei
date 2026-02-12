fa6466ba5170d940819beb86e130cbe5b263bf11
James Ashdown <260547476+james-ashdown@users.noreply.github.com>
2026-02-12 16:39:01 -0700

    feat: impl Debug for I32F

--- src/i32f.rs
+++ src/i32f.rs
@@ -1,3 +1,5 @@
+use ::core::fmt;
+
 pub struct I32F<const E: i32>(pub(crate) i32);
 
 impl<const E: i32> I32F<E> {
@@ -11,5 +13,12 @@ impl<const E: i32> I32F<E> {
 
     pub const fn exponent(self) -> i32 {
         E
+    }
+}
+
+impl<const E: i32> fmt::Debug for I32F<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "I32F<{E}")?;
+        f.debug_tuple(">").field(&self.0).finish()
     }
 }
