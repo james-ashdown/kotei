a750bce1140527d90ad28231099b60edd0a3566a
James Ashdown <260547476+james-ashdown@users.noreply.github.com>
2026-02-12 17:04:47 -0700

    docs: add doc comments to all existing items, add #[warn(missing_docs)] lint

--- src/i32f.rs
+++ src/i32f.rs
@@ -1,17 +1,21 @@
 use ::core::fmt;
 
+/// The 32-bit signed fixed-point type.
 #[derive(Clone, Copy, Hash)]
 pub struct I32F<const E: i32>(pub(crate) i32);
 
 impl<const E: i32> I32F<E> {
+    /// Creates a new fixed-point number from an integer significand, equal to `significand` ⋅ 2<sup>E</sup>.
     pub const fn new(significand: i32) -> Self {
         Self(significand)
     }
 
+    /// Returns the fixed-point significand, equal to `self` ⋅ 2<sup>-E</sup>.
     pub const fn significand(self) -> i32 {
         self.0
     }
 
+    /// Returns the fixed-point exponent.
     pub const fn exponent(self) -> i32 {
         E
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! `kotei` is a human-readable, no-dependencies `no_std` fixed-point arithmetic library.
+
+#![warn(missing_docs)]
 #![no_std]
 
 mod i32f;
